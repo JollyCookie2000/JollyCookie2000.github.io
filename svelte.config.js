557594314cf5957320c7e634b14d81aef739fea9
lorenzo <d00m373rnal@protonmail.com>
2025-01-07 22:04:38 +0100

    Configure for static building

--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,14 @@ const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
+		// https://svelte.dev/docs/kit/adapter-static
+		adapter: adapter({
+			pages: 'build', // Target build directory
+			assets: 'build', // Target build directory
+			fallback: null, // Fallback page in case of error
+			precompress: false, // Pre-compress files with brotli and gzip
+			strict: true
+		})
 	}
 };
 
