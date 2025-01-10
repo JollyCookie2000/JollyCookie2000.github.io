0fd6df7da48d3affbf8571e7ed71a40af327c10e
lorenzo <lorenzo.adam.piazza@protonmail.ch>
2025-01-10 23:47:38 +0100

    Display app icons

--- src/lib/AppLauncher.svelte
+++ src/lib/AppLauncher.svelte
@@ -1,5 +1,30 @@
 <script lang="ts">
-
+	import AppIcon from "$lib/AppIcon.svelte";
 </script>
 
-<h1>App launcher</h1>
+<div class="apps">
+	{#each { length: 5 } as app}
+		<AppIcon display_name="App" />
+	{/each}
+</div>
+
+<style>
+	* {
+		border-width: 0px;
+		box-sizing: border-box;
+		margin: 0px;
+		padding: 0px;
+	}
+
+	.apps {
+		align-content: flex-start;
+		align-items: flex-start;
+		display: flex;
+		flex-wrap: wrap;
+		gap: 4rem;
+		height: 100%;
+		justify-content: center;
+		padding: 1rem;
+		width: 100%;
+	}
+</style>
